987a46e3513db50e2c51e40e98de0d807593dd8d
zxg321 <zxg321@qq.com>
2018-09-10 14:06:36 +0800

    CMS

--- src/ZcmsServiceProvider.php
+++ src/ZcmsServiceProvider.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Zxg321\ZCms;
+namespace Zxg321\Zcms;
 
 use Illuminate\Support\ServiceProvider;
 
@@ -9,6 +9,12 @@ class ZcmsServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->loadViewsFrom(__DIR__.'/../resources/views', 'zcms');
+        $this->publishes([
+            __DIR__.'/../config/zcms.php' => config_path('zcms.php')
+        ], 'zcms-config');
+        $this->publishes([
+            __DIR__.'/Database/migrations/' => database_path('migrations')
+        ], 'zcms-migrations');
     }
     public function register()
     {
